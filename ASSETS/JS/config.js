526e7a5f678c7a540ce6f8788f198c879562b71c
variowhite <variowhite@outlook.com>
2020-06-13 05:33:00 +0700

    DOM #Pricing

--- ASSETS/JS/config.js
+++ ASSETS/JS/config.js
@@ -1,9 +1,14 @@
 //EDIT HERE
-var GooglePlay         =    "https://play.google.com/store";
-var FaceBook           =    "https://www.facebook.com/";
-var Twitter            =    "https://twitter.com/";
-var Picasa             =    "https://picasa.google.com/";
-var GooglePlus         =    "https://myaccount.google.com/profile";
+let GooglePlay         =    "https://play.google.com/store";
+let FaceBook           =    "https://www.facebook.com/";
+let Twitter            =    "https://twitter.com/";
+let Picasa             =    "https://picasa.google.com/";
+let GooglePlus         =    "https://myaccount.google.com/profile";
+
+let SingUp_Standart    =    "https://myaccount.google.com/profile";
+let SingUp_Business    =    "https://myaccount.google.com/profile";
+let SingUp_Premium     =    "https://myaccount.google.com/profile";
+let SingUp_Extra       =    "https://myaccount.google.com/profile";
 
 
 //LINKS-------/////////////////////////////////////////////////////
@@ -12,10 +17,14 @@ UpdateLinks(".Button_FB", FaceBook);
 UpdateLinks(".Button_TW", Twitter);
 UpdateLinks(".Button_PC", Picasa);
 UpdateLinks(".Button_GP", GooglePlus);
+UpdateLinks(".Button_SING_UP_S", SingUp_Standart);
+UpdateLinks(".Button_SING_UP_B", SingUp_Business);
+UpdateLinks(".Button_SING_UP_P", SingUp_Premium);
+UpdateLinks(".Button_SING_UP_E", SingUp_Extra);
 
 function UpdateLinks(_class, _link){
     let buttons = document.querySelectorAll(_class);
-    for (var i = 0; i < buttons.length; i++){
+    for (let i = 0; i < buttons.length; i++){
         buttons[i].href=_link;
     }
 }
